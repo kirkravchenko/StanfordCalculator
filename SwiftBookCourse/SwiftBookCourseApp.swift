929e6797ecf9d8c63e931381d823954c7ff87eb0
kirkravchenko <kyrylkravchenko1994@gmail.com>
2021-10-28 12:19:40 +0300

    Initial Commit

---
+++ SwiftBookCourse/SwiftBookCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBookCourseApp.swift
+//  SwiftBookCourse
+//
+//  Created by Kyrylo Kravchenko on 28.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBookCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
