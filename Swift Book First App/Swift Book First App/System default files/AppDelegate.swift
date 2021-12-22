551c8ce8fd19c08774e1ccce027a3b4509c824f9
kirkravchenko <kyrylkravchenko1994@gmail.com>
2021-12-22 11:58:32 +0200

    master view on launch

--- Swift Book First App/Swift Book First App/System default files/AppDelegate.swift
+++ Swift Book First App/Swift Book First App/System default files/AppDelegate.swift
@@ -9,11 +9,24 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.main.bounds)
+
+        let masterController = CalculatorViewController()
+        let navMasterController = UINavigationController(rootViewController: masterController)
+        let detailController = GraphViewController()
+        let navDetailController = UINavigationController(rootViewController: detailController)
+
+        let splitController = UISplitViewController()
+        splitController.viewControllers = [navMasterController, navDetailController]
+        splitController.preferredDisplayMode = UISplitViewController.DisplayMode.oneBesideSecondary
+        splitController.delegate = self
+        self.window?.rootViewController = splitController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
@@ -30,7 +43,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
 }
 
+extension AppDelegate: UISplitViewControllerDelegate {
+
+    func splitViewController(
+        _ splitViewController: UISplitViewController,
+        collapseSecondary secondaryViewController: UIViewController,
+        onto primaryViewController: UIViewController) -> Bool {
+            return true
+    }
+}
